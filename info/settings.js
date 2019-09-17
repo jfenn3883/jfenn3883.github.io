f766449d089d9c0b5fc83ac46063b394fd06489b
jfenn3883 <jfenn2002@gmail.com>
2019-09-17 13:16:01 -0500

    adding fonts and settings files

---
+++ info/settings.js
@@ -0,0 +1,29 @@
+let font_impact = loadFont('impact.ttf');
+let font_ubuntu = loadFont('Ubuntu.ttf');
+
+let fonts = {
+  impact: font_impact,
+  ubuntu: font_ubuntu
+};
+
+let colors = {
+  red: 'rgb(255, 0, 0)',
+  orange: 'rgb(255, 128, 0)',
+  yellow: 'rgb(255, 255, 0)',
+  yellow_green: 'rgb(191, 255, 0)',
+  green: 'rgb(64, 255, 0)',
+  green_blue: 'rgb(0, 255, 128)',
+  aqua: 'rgb(0, 255, 255)',
+  blue: 'rgb(0, 64, 255)',
+  purple: 'rgb(191, 0, 255)',
+  pink: 'rgb(255, 0, 191)',
+  black: 'rgb(0, 0, 0)',
+  white: 'rgb(255, 255, 255)'
+};
+
+let settings = {
+  background_color: colors.black,
+  text_color: colors.white,
+  font_: fonts.ubuntu,
+  text_size: 24,
+};
--- main.js
+++ main.js
@@ -1,9 +1,22 @@
+
+// a website using p5 to track students during emergancies.
+
+new p5();
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
 }
 
 function draw() {
-  background(0);
+  background(settings.background_color);
+}
+
+function setTextColors() {
+  textFont(settings.font_);
+  textSize(settings.text_size);
+  textAlign(CENTER, CENTER);
+  fill(settings.text_color);
 }
 
 function windowResized() {
