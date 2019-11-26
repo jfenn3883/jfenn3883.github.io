764b4b5445247801e612b011835e134e7b705c8d
jfenn3883 <45019671+jfenn3883@users.noreply.github.com>
2019-11-26 14:20:32 -0600

    Update 404.js

--- pages/404/404.js
+++ pages/404/404.js
@@ -10,12 +10,12 @@ function draw() {
   textFont(settings.font_, 100);
   fill(colors.white);
   textAlign(CENTER, CENTER);
-  text("404 Error", windowWidth / 2, 150 + windowHeight / 5);
+  text("404", windowWidth / 2, 150 + windowHeight / 5);
   
   textFont(settings.font_, 24)
   fill(colors.white);
   textAlign(CENTER, CENTER);
-  text("sowwy, we couwd not find the wequested wink", windowWidth / 2, 200 + windowHeight / 5);
+  text("sowwy, we couwd not find the wequested wink", windowWidth / 2, 210 + windowHeight / 5);
 }
 
 function windowResized() {
