2a464ad11c28ffc8e1906e292f279761bddc899c
jfenn3883 <45019671+jfenn3883@users.noreply.github.com>
2019-11-26 13:46:38 -0600

    adding the 404 text
    finally

--- pages/404/404.js
+++ pages/404/404.js
@@ -1,13 +1,15 @@
 // manages the 404 page
 
-let canvas404; // creating a global canvas
-
 function setup() {
-  canvas404 = createCanvas(windowWidth, windowHeight);
+  let canvas404 = createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(colors.black)
+  background(colors.black);
+  textSize(60);
+  fill(colors.white);
+  textAlign(CENTER, CENTER);
+  text("404", windowWidth / 2, windowHeight / 5);
 }
 
 function windowResized() {
