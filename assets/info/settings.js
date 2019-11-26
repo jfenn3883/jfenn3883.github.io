950d869a95c53c2e461f9f8e6e076623aec05191
jfenn3883 <45019671+jfenn3883@users.noreply.github.com>
2019-11-26 13:25:03 -0600

    updating at the end of class
    the code cant find canvas 404 in the windowResized function. i need to make it global, but im not sure this is the correct behavior.

--- assets/info/settings.js
+++ assets/info/settings.js
@@ -1,12 +1,12 @@
 new p5();
 
-let font_impact = loadFont('info/fonts/impact.ttf');
+/* let font_impact = loadFont('info/fonts/impact.ttf');
 let font_ubuntu = loadFont('info/fonts/Ubuntu.ttf');
 
 let fonts = {
   impact: font_impact,
   ubuntu: font_ubuntu
-};
+}; */
 
 let colors = {
   red: 'rgb(255, 0, 0)',
@@ -26,6 +26,6 @@ let colors = {
 let settings = {
   background_color: colors.white,
   text_color: colors.black,
-  font_: fonts.ubuntu,
+  //font_: fonts.ubuntu,
   text_size: 24
 };
--- pages/404/404.js
+++ pages/404/404.js
@@ -1,9 +1,15 @@
-new p5();
+// manages the 404 page
+
 function setup() {
   let canvas404 = createCanvas(windowWidth, windowHeight);
-  canvas404.background(colors.black);
+  canvas404.background(colors.black)
 }
 
 function draw() {
 
+}
+
+function windowResized() {
+  canvas404.resizeCanvas(windowWidth, windowHeight);
+  canvas404.background(colors.black)
 }
