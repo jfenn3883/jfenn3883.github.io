dc6d341fab3f79fad9798f330c463faeb369d7c3
Jacob Fenn <jfenn2002@gmail.com>
2019-11-28 13:11:11 -0600

    added comments to 404

--- pages/404/404.js
+++ pages/404/404.js
@@ -1,23 +1,23 @@
 // manages the 404 page
 
 function setup() {
-  let canvas404 = createCanvas(windowWidth, windowHeight);
+  let canvas404 = createCanvas(windowWidth, windowHeight); // creates a canvas
 }
 
 function draw() {
-  background(colors.offblack);
-  
-  textFont(settings.font_, 100);
-  fill(colors.white);
-  textAlign(CENTER, CENTER);
-  text("404", windowWidth / 2, 150 + windowHeight / 5);
-  
+  background(settings.background_color); // changes the background color to what is set in assets/info/settings.js (defaults to offblack)
+
+  textFont(settings.font_, 100); // sets font to what is in assets/info/settings.js (defaults) to white and text size to 100
+  fill(settings.text_color); // sets background color to what is in assets/info/settings.js (defaults) to white
+  textAlign(CENTER, CENTER); // sets the alignment of the texbox
+  text("404", windowWidth / 2, 150 + windowHeight / 5); // places the "404" text in the center of the page horizontally, and a custom aliment vertically
+
   textFont(settings.font_, 24)
-  fill(colors.white);
+  fill(settings.text_color);
   textAlign(CENTER, CENTER);
   text("sowwy, we couwd not find the wequested wink", windowWidth / 2, 230 + windowHeight / 5);
 }
 
-function windowResized() {
+function windowResized() { // a function that
   resizeCanvas(windowWidth, windowHeight);
 }
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,7 +1,11 @@
 function setup() {
-
+  let canvasIndex = createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  
+  background(settings.background_color);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
