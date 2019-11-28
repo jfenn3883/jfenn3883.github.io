41049ee21c8295f9cd91300b4152d439d0e28791
Jacob Fenn <jfenn2002@gmail.com>
2019-11-28 14:25:50 -0600

    getting off for now

--- pages/404/404.js
+++ pages/404/404.js
@@ -1,20 +1,31 @@
 // manages the 404 page
+let canvas404
+let back_button // creates the back back_button
 
 function setup() {
-  let canvas404 = createCanvas(windowWidth, windowHeight); // creates a canvas
+  canvas404 = createCanvas(windowWidth, windowHeight); // creates a canvas
 
-  let back_button = new Clickable(); // creates the back back_button
+  back_button = new Clickable(); // ensure changes to these are replicated in windowResized
   back_button.locate(windowWidth / 2, 250 + windowHeight / 5); // puts the button in the right place
   back_button.resize(2 * windowWidth / 6, windowHeight / 10); // makes the button the right size
-  back_button.color = settings.offwhite; // color of the button
   back_button.cornerRadius = 2; // the radius of the corner
-  back_button.textColor = settings.offblack; // sets the text color
   back_button.text = "Go Back"; // sets the text
 
-  //This function is ran when the clickable is hovered but not pressed.
+  // this function is ran when the clickable is hovered but not pressed.
   back_button.onHover = function() {
-    this.color = settings.offwhite;
-    this.textColor = settings.offblack;
+    this.color = colors.offwhite;
+    this.textColor = colors.offblack;
+  }
+
+  // this function is ran when the clickable is not hovered
+  back_button.onOutside = function() {
+    back_button.color = colors.offwhite; // color of the button
+    back_button.textColor = colors.offblack; // sets the text color
+  }
+
+  // this function is run when the button is released
+  back_button.onRelease = function() {
+    
   }
 }
 
@@ -31,8 +42,13 @@ function draw() {
   textAlign(CENTER, CENTER);
   text("sowwy, we couwd not find the wequested wink", windowWidth / 2, 180 + windowHeight / 5);
 
+  back_button.draw();
 }
 
 function windowResized() { // a function that resizes the canvas if it changes size
   resizeCanvas(windowWidth, windowHeight);
+
+  // puts the button back in the right place if it is resized
+  back_button.locate(windowWidth / 2, 250 + windowHeight / 5);
+  back_button.resize(2 * windowWidth / 6, windowHeight / 10);
 }
